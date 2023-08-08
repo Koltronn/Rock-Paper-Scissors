f644b7ba4ecfd29c18c2a80efc4dbb9ffb57b73e
Koltronn <74987336+Koltronn@users.noreply.github.com>
2023-08-08 13:04:04 -0500

    Added DOM manipulation

--- main.js
+++ main.js
@@ -3,59 +3,64 @@ let cpuRandChoice;
 let playerChoice;
 let playerScore = 0;
 let cpuScore = 0;
+
+div = document.querySelector('div');
+roundMsg = document.createElement('h1');
+scoreMsg = document.createElement('h2');
+winLossMsg = document.createElement('h3');
+div.appendChild(roundMsg);
+div.appendChild(scoreMsg)
+div.appendChild(winLossMsg)
+let buttons = document.querySelectorAll('button')
+
+
+
 function getComputerChoice() {
     cpuRandChoice = choices[Math.floor(Math.random() * 3)];
     return cpuRandChoice;
-}
-
-function playerSelection() {
-    playerChoice = prompt("Rock, paper or scissors?").toLowerCase();
-    console.log(playerChoice);
-    if (playerChoice === 'rock' 
-    || playerChoice === 'paper'
-    || playerChoice === 'scissors') {
-        return playerChoice;
-    } else {
-        alert("not a valid choice")
-        playerSelection();
-    }
 }
 
 function round(playerSelection, getComputerChoice) {
     if ((playerSelection === 'rock' && getComputerChoice === 'paper') 
     || (playerSelection === 'paper' && getComputerChoice === 'scissors')
     || (playerSelection === 'scissors' && getComputerChoice === 'rock')) {
-        console.log(`YOU LOSE, ${getComputerChoice.toUpperCase()} BEATS ${playerSelection.toUpperCase()}`)
+        roundMsg.textContent = `YOU LOSE, ${getComputerChoice.toUpperCase()} BEATS ${playerSelection.toUpperCase()}`
         cpuScore += 1;
-        console.log(`YOUR SCORE:${playerScore}
-        COMPUTER'S SCORE: ${cpuScore}`)
+        scoreMsg.textContent = `YOUR SCORE:${playerScore}\n
+        COMPUTER'S SCORE: ${cpuScore}`
     } else if ((playerSelection === 'rock' && getComputerChoice === 'scissors') 
     || (playerSelection === 'paper' && getComputerChoice === 'rock')
     || (playerSelection === ' scissors' && getComputerChoice === 'paper')) {
-        console.log(`YOU WIN, ${playerSelection.toUpperCase()} BEATS ${getComputerChoice.toUpperCase()}`)
+        roundMsg.textContent = `YOU WIN, ${playerSelection.toUpperCase()} BEATS ${getComputerChoice.toUpperCase()}`
         playerScore += 1;
-        console.log(`YOUR SCORE:${playerScore}
-        COMPUTER'S SCORE: ${cpuScore}`)
-    } else {
-        console.log(`IT'S A TIE! YOU BOTH CHOSE ${playerSelection.toUpperCase()}`)
-        console.log(`YOUR SCORE:${playerScore}
-        COMPUTER'S SCORE: ${cpuScore}`)
+        scoreMsg.textContent = `YOUR SCORE:${playerScore}
+        COMPUTER'S SCORE: ${cpuScore}`
+    } else if (playerSelection === getComputerChoice){
+        roundMsg.textContent = `IT'S A TIE! YOU BOTH CHOSE ${playerSelection.toUpperCase()}`
+        scoreMsg.textContent =  `YOUR SCORE:${playerScore}
+        COMPUTER'S SCORE: ${cpuScore}`
     }
 }
 
-
 function game() {
-    while (playerScore < 3 || cpuScore < 3) {
-        round(playerSelection(), getComputerChoice());
-        if (playerScore === 3) {
-            alert("PLAYER WINS THE GAME")
-            break;
-        } else if (cpuScore === 3) {
-            alert("CPU WINS THE GAME")
-            break;
-        }
-    }
+    buttons.forEach((button => {
+        button.addEventListener('click', function(e){
+            round(e.target.textContent.toLowerCase(), getComputerChoice())
+            if (playerScore === 5) {
+                winLossMsg.textContent = "PLAYER WINS THE GAME";
+                playerScore = 0;
+                cpuScore = 0;
+                roundMsg.textContent = "";
+                scoreMsg.textContent = "";
+            } else if(cpuScore === 5) {
+                winLossMsg.textContent = "CPU WINS THE GAME";
+                playerScore = 0;
+                cpuScore = 0;
+                roundMsg.textContent = "";
+                scoreMsg.textContent = "";
+            }
+        })
+    }))
 }
 
 game();
-
